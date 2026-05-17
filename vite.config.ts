6d127665a25bb0c017139fbfe065d44c253a134b
mrtzfchs <mrtzfchs@gmail.com>
2026-05-17 13:22:05 +0200

    Replace Sail-specific Wayfinder command with generic PHP Artisan command
    Signed-off-by: mrtzfchs <mrtzfchs@gmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
         }),
         wayfinder({
             formVariants: true,
-            command: './vendor/bin/sail artisan wayfinder:generate',
+            command: 'php artisan wayfinder:generate',
         }),
     ],
 });
